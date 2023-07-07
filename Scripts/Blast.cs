ed8ff735a9016403ef5cce299d2e174df0205a68
Soulst <36247278+Soulst@users.noreply.github.com>
2023-07-07 09:59:36 -0700

    Add files via upload

---
+++ Scripts/Blast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Blast : MonoBehaviour
+{
+    public Transform laserSpawn;
+    public float nextLaser = 0.7f;
+    public float currTime = 0.0f;
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserSpawn = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blast();
+    }
+    public void blast()
+    {
+        currTime += Time.deltaTime;
+        if(Input.GetButton("Fire1") && currTime > nextLaser)
+        {
+            nextLaser += currTime;
+            Instantiate (projectile,laserSpawn.position, Quaternion.identity);
+            nextLaser -= currTime;
+            currTime = 0.0f;
+        }
+    }
+}
---
+++ Scripts/MonsterThreeBlast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterThreeBlast : MonoBehaviour
+{
+    public Transform enemyLaserSpawn;
+    public float nextLaser = 1.0f;
+    public float currTime = 0.0f;
+    public GameObject enemyLaser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyLaserSpawn = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyBlast();
+    }
+    public void enemyBlast()
+    {
+        currTime += Time.deltaTime;
+        if (currTime > nextLaser)
+        {
+            nextLaser += currTime;
+            Instantiate(enemyLaser, enemyLaserSpawn.position, Quaternion.identity);
+            nextLaser -= currTime;
+            currTime = 0.0f;
+        }
+
+    }
+
+}
